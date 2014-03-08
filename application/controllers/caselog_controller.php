06b448e9dac3f0b963c80cd47d2f03180977e5d0
paulyoingco <piyo0508@yahoo.com>
2014-03-09 00:28:13 +0800

    qwewqe

--- application/controllers/caselog_controller.php
+++ application/controllers/caselog_controller.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-//palitan
+//asdasdasdasdas/
 session_start(); //we need to call PHP's session object to access it through CI
 class Caselog_controller extends CI_Controller {
 	function __construct()
