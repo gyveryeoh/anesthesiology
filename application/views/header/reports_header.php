833f4a80d63e7356099e284451ea8591bb59e3b3
paulyoingco <piyo0508@yahoo.com>
2014-03-19 05:24:17 +0800

    nilagay ko na yung menu para sa superuser gawa ka nalang ng superuser

--- application/views/header/reports_header.php
+++ application/views/header/reports_header.php
@@ -2,7 +2,14 @@
     <tr>
         <td align="right" class="border-less" style="background-color:#FFF5EE; font-family: sans-serif;font-size: 10px;font-weight:bold;">
         <a href="<?php echo base_url();?>index.php/reports_controller/anesth_services">ANESTHETIC SERVICES REPORT</a> - 
+		<?php if ($user_information['role_id'] == "3")
+		{
+		?>
         <a href="<?php echo base_url();?>index.php/reports_controller/institution_view">INSTITUTION SUMMARY REPORT</a>-
+        <a href="<?php echo base_url();?>index.php/users_controller/hospital_list">HOSPITAL LIST</a>-
+		<?php 
+		}
+		?>
         <a href="<?php echo base_url();?>index.php/reports_controller/">ANESTHETIC SUMMARY REPORT</a> - 
         <a href="<?php echo base_url();?>index.php/users_controller/users_caselog">ENCODED SUMMARY</a>
         </td>
