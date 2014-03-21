85ef60e4a9d11ddd77798361e1b83cd4873b3225
paulyoingco <piyo0508@yahoo.com>
2014-03-21 12:31:06 +0800

    added button deactivation activation

--- application/views/users/hospital_list.php
+++ application/views/users/hospital_list.php
@@ -2,7 +2,7 @@
 <table width="80%" cellpadding="1" cellspacing="0">
           <tr>
            <th width="50%">Institution Name</th>
-           <th>Users</th>
+           <th colspan="4">Users</th>
           </tr>
           <?php
 		  $index = 0;
@@ -11,8 +11,8 @@
 				<td>
 					$row->name
 				</td>
-				<td>
-				<select name='user' style='width:400px;' id='user_id$index'>
+				<td width='10%'>
+				<select name='user' style='width:300px;' id='user_id$index' onChange='act_dec($index)'>
 				<option value=''>Select User</option>";
 				foreach($user_per_hospital[$row->id] as $row2)
 				{
@@ -20,12 +20,26 @@
 				}
 			echo "</select>";
 			?>
-				<button onclick="report_list(<?php echo $index?>,1)">View Reports</button>
-				<button onclick="report_list(<?php echo $index?>,2)">Edit Profile</button>
-				<button onclick="report_list(<?php echo $index?>,3)">Deactivate</button>
-				<div id="mess<?php echo $index?>" style="color:red"></div>
-			<?php echo "</td></tr>";
-			$index++;
+			<?php echo "</td>";
+			?>
+				<td width='10%'>
+					
+					<button onclick="report_list(<?php echo $index?>,1)">View Reports</button>
+				
+				</td>
+				
+				<td width='10%'>
+				
+					<button onclick="report_list(<?php echo $index?>,2)">Edit Profile</button>
+				
+				</td>
+				
+				<td width='10%' id="activate_deactivate<?php echo $index;?>" style="color:red;">
+					
+				</td>
+		  <?php
+			echo "</tr>";
+		  $index++;
 		  }
           ?>
            <tr>
@@ -35,9 +49,23 @@
             <td colspan="2" align="center" class="border-less"><br><br><br>Copyright 2013 PBA - Philippine Board of Anesthesiology</td>
            </tr>
 </table>
+<script src="<?php echo base_url() ?>assets/javascript/jquery.js" type="text/javascript"></script>
 <script>
-    function report_list($index,$button)
+
+	function act_dec($index)
 	{
+		var user_id = $("#user_id"+$index).val();
+		$.ajax({
+			type : "POST",
+			url  : "<?php echo base_url(); ?>index.php/reports_controller/activate_deactivate",
+			data : "user_id=" + user_id + "&index=" + $index,
+			success: function(data){
+			   $("#activate_deactivate"+$index).html(data);
+		}
+		});
+	}
+	function report_list($index,$button)
+	{				
 		var user = $("#user_id"+$index).val();
 		if(user != '')
 		{
@@ -49,14 +77,32 @@
 			{
 				document.location.href='<?php echo base_url(); ?>index.php/users_controller/edit_user?resident_id='+user;
 			}
-			else
-			{
-				alert("fuck");
-			}
+			
 		}
 		else
 		{
-			$("#mess"+$index).html("Select User");
+			$("#activate_deactivate"+$index	).html("Select User");
 		}
+		
+	}
+	
+	function a_d($user_id,$index,$id)
+	{
+			$.ajax({
+				type : "POST",
+				url  : "<?php echo base_url(); ?>index.php/reports_controller/execute",
+				data : "user_id=" + $user_id + "&id=" + $id,
+				success: function(data){
+				if($id == 1)
+				{
+					alert("Account Deactivated");
+				}
+				else
+				{
+					alert("Account Activated");
+				}
+				act_dec($index);
+			}
+			});
 	}
 </script>
