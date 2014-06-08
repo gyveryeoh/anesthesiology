2101720f315115d7c5082f8a136076142048037f
paulyoingco <piyo0508@yahoo.com>
2014-06-09 00:33:05 +0800

    may binura lang na extra codes.

--- application/controllers/reports_controller.php
+++ application/controllers/reports_controller.php
@@ -567,9 +567,6 @@ function annual_patient_classification_and_distribution_summary()
         $session_data = $this->session->userdata('logged_in');
         $data["user_information"] = $session_data;
 		$data["anesth_institutions"] = $this->dropdown_select->anesth_institutions();
-		$data["institution_id"] = 0;
-		$data["year_level"] = 0;
-		$data["year"] = 0;
 		
 		$institution_id = 0;
 		$year_level = 0;
@@ -586,7 +583,6 @@ function annual_patient_classification_and_distribution_summary()
 			$data["year_level"] = $year_level;
 			$data["year"] = $year;
 		}
-		
 		
 		if($institution_id == 0)
 		{
