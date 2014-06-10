441837c815180cbe77df727356a7582732e62762
paulyoingco <piyo0508@yahoo.com>
2014-06-11 00:30:46 +0800

    hospitals/services added

---
+++ application/views/reports/anesth_hospital_and_service.php
@@ -0,0 +1,60 @@
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/javascript/jquery.js"></script>
+<script>
+$(document).ready(function() {
+$('#selectall').click(function(event) {   
+    if(this.checked) {
+        // Iterate each checkbox
+        $(':checkbox').each(function() {
+            this.checked = true;                        
+        });
+    }
+	else{
+		$(':checkbox').each(function() { //loop through each checkbox
+			this.checked = false; //deselect all checkboxes with class "checkbox1"                       
+		});         
+	}
+});
+});
+</script>
+<form method="post" action="<?php echo base_url(); ?>index.php/reports_controller/anesth_hospital_and_service"/>
+<table>
+<tr>
+<td align="center">
+<input type="submit" name="submit" value="submit"/>
+<select name="institution_id">
+<option value = 0> ALL </option>
+  <?php
+	foreach($anesth_institutions as $ai):
+  ?>
+  <option value="<?php echo $ai->id?>" <?php if ($ai->id == $institution_id) { echo 'selected="selected"'; }?>><?php echo $ai->name?></option>
+  <?php
+  endforeach;
+  ?>
+</select>
+</td>
+</tr>
+<tr>
+<td>
+<input type="checkbox" id="selectall">Select All
+<?php
+	echo "</br>";
+	foreach($anesth_services as $as)
+	{	
+		echo '<input type="checkbox" name="service_id[]" value = "'.$as->id.'"';
+		if($services != NULL)
+		{
+			foreach($services as $key=>$value)
+			{
+				if($as->id == $value)
+				{
+					echo "checked";
+				}
+			}
+		}
+		echo ">".$as->name."</br>";
+	}
+?>
+</td>
+</tr>
+</table>
+</form>
